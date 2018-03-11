3b478bacc3e31c8e359c1ca4a0c26b480352fe27
mine_mager <1061340144@qq.com>
2018-03-11 16:33:30 +0800

    20170416

--- E_Life/js/interaction/index.js
+++ E_Life/js/interaction/index.js
@@ -1,4 +1,4 @@
-function get_serve_info(){
+function get_user_info(){
 	$.ajax(  
     {  
         type:'get',  
@@ -30,4 +30,52 @@ function is_login(user){
 function not_login(){
 	$('#login_btn').css('display','block');
 	$('#loginuser').css('display','none');
+}
+
+function get_order_info(){
+	$.ajax(  
+    {  
+        type:'get',  
+        url : 'http://elife.com/show_order.php?',  
+        dataType : 'jsonp',  
+        jsonp:"jsoncallback", 
+        timeout:15000,
+        success  : function(data) { 
+            if(data.ret){
+            	for(var i=0;i<data.orderinfo.length;i++){
+            		var status=data.orderinfo[i]['status'] == 0 ? '进行中' : '已完成';
+            		create_order(data.serveinfo[i][0]['serve_title'],status,data.serveinfo[i][0]['serve_content'],data.orderinfo[i]['cost'],data.serveinfo[i][0]['big_icon']);
+            	}
+            }
+        },  
+        error : function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log(XMLHttpRequest.readyState);
+            console.log(XMLHttpRequest.status);
+            console.log(textStatus);
+            ELife_UI.Toast.show('服务器繁忙，请稍后重试！');
+			window.setTimeout(function(){
+				ELife_UI.Toast.hide();
+			},2000);
+        }  
+    }  
+); 
+}
+
+function create_order(name,status,desc,money,url){
+	var innerhtml='<div class="order_div1">'+
+					'<div class="order_name">'+name+'</div>'+
+					'<div class="order_status">'+status+'</div>'+
+				'</div>'+																								
+				'<div class="order_div2">'+
+					'<img class="order_img" src=../../debug/images/'+url+' />'+
+					'<div class="order_desc">'+desc+'</div>'+
+				'</div>'+
+				'<div class="order_money">合计：￥'+money+'</div>'+
+				'<div class="order_judge">'+
+					'<a href="#">评价</a>'+
+				'</div>';
+	var orderitem=document.createElement('section');
+	orderitem.className='orderbox';
+	orderitem.innerHTML=innerhtml;
+	$('#all_order').append(orderitem);
 }
--- elife_php/show_order.php
+++ elife_php/show_order.php
@@ -3,13 +3,15 @@
 
     session_start();
 
+    $userid=$_SESSION['userid'];
+
     $data = ["ret"=>false, "msg"=>"","orderinfo"=>'',"serveinfo"=>''];
 
-    $data = get_order();
+    $data = get_order($userid);
 
     echo $_GET['jsoncallback']."(".json_encode($data).")";//解决ajax跨域问题
 
-    function get_order(){
+    function get_order($userid){
     	/*$hostname = "139.199.198.216";
 	    $dbname = "elife";
 	    $username = "Administrator";
@@ -25,11 +27,20 @@
             $_opts_values = array(PDO::ATTR_PERSISTENT=>true,PDO::ATTR_ERRMODE=>2,PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8');//解决乱码问题
 	        $conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password,$_opts_values);
 	        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	        $sql = "select * from elife_order where user_id=$_SESSION['userid']";
+	        $sql = "select * from elife_order where user_id=$userid order by _id desc";
 	        $stmt = $conn->prepare($sql);
 	        $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
             if (count($result)){
+                $data['orderinfo']=$result;
+                foreach($result as $key=>$value){
+                    $s_id=$value['serve_id'];
+                    $sql = "select * from elife_serve where _id=$s_id";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute();
+                    $jieguo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    $data['serveinfo'][$key]=$jieguo;
+                }
                 $data['ret']=true;
             }else{
                 $data['ret']=false;
