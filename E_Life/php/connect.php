dcc9cf4b77f79e213bb5c88bed63b9abefe91271
mine_mager <1061340144@qq.com>
2018-03-11 15:47:33 +0800

    20170324

--- E_Life/php/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$conn=mysql_connect("localhost","root","") or die("数据库服务器连接失败".mysql_error());
-	mysql_select_db("elife",$conn) or die("数据库连接失败".mysql_error);
-	mysql_query("set names utf8");
-?>
