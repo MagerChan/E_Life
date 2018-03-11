cefa6d655e7ac45cca77c5dbec0da71682dc5ea5
mine_mager <1061340144@qq.com>
2018-03-11 18:00:38 +0800

    2017051401

--- elife_php/logout.php
+++ elife_php/logout.php
@@ -3,14 +3,24 @@
 
     session_start();
 
-    $_SESSION['tel']='';
-    $_SESSION['userid']='';
-    $_SESSION['servename']='';
-    $_SESSION['servecost']='';
-    $_SESSION['addrinfo']='';
-    $_SESSION['serveid']='';
-    $_SESSION['addrid']='';
-    $data = ["ret"=>true];
+    $data = ["ret"=>false,'islogin'=>true];
+
+    if($_SESSION['tel']){
+        $_SESSION['tel']='';
+        $_SESSION['userid']='';
+        $_SESSION['servename']='';
+        $_SESSION['servecost']='';
+        $_SESSION['addrinfo']='';
+        $_SESSION['serveid']='';
+        $_SESSION['addrid']='';
+        $data['islogin']=true;
+        $data['ret']=true;
+    }else{
+        $data['islogin']=false;
+        $data['ret']=false;
+    }
+    
+    
 
     echo $_GET['jsoncallback']."(".json_encode($data).")";//解决ajax跨域问题
 ?>
