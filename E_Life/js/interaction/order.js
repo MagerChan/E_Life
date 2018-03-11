ae45f05e0b00c38cc1b2e326b7a9fcc71e115d95
mine_mager <1061340144@qq.com>
2018-03-11 16:10:25 +0800

    20170408

---
+++ E_Life/js/interaction/order.js
@@ -0,0 +1,9 @@
+$('#ordernow').click(function(){
+//	if(username == ""){
+//		location.href="../../debug/tmpl/login.html";
+//	}else{
+//		location.href="../../debug/tmpl/place_order.html";
+//	}
+
+	location.href="../../debug/tmpl/place_order.html";
+});
